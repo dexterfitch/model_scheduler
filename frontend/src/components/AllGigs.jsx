326324add4dad5e2bfe8913a50a717d28dd001b3
Dex Fitch <fitch.dexter@gmail.com>
2026-07-01 09:42:18 -0400

    And another another another another one

--- frontend/src/components/AllGigs.jsx
+++ frontend/src/components/AllGigs.jsx
@@ -100,7 +100,7 @@ function AllGigs() {
         </InputGroup>
       </div>
 
-      <div className="p-3 bg-light rounded mb-4 d-flex align-items-end date-filter-box">
+      <div className="p-3 border-bottom border-light mb-4 d-flex align-items-end date-filter-box">
         <Row className="g-2 align-items-end w-100">
           <Col xs={12} md="auto">
             <Button
